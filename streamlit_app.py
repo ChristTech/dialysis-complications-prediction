52bdbe36ce505fa250df7a10b7cc60c8740944ea
adebisi victor <adebisivictor39@gmail.com>
2025-05-12 19:44:48 +0100

    library update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,17 @@ import numpy as np
 import joblib
 from PIL import Image
 import base64
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+# Define neural network
+def build_nn_model():
+    model = Sequential()
+    model.add(Dense(64, activation='relu', input_shape=(10,))) # Input shape needs to match your data
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))  # Binary classification
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
 
 # Load voting model and scaler
 try:
